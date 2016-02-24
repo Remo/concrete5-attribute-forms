8c494cafc2007c1eff0454d403243dfabc052f38
avdevs <bhavin@avdevs.com>
2016-02-24 12:20:00 +0530

    HTML Decode for get proper email message
    HTML Decode for get proper email message

--- mail/attribute_form_admin_notif.php
+++ mail/attribute_form_admin_notif.php
@@ -6,19 +6,19 @@ $aft           = $af->getTypeObj();
 
 $formDisplayUrl = URL::to('dashboard/forms/results/entries', $aft->getID());
 
-$body = t("
+$body = html_entity_decode(t("
 There has been a submission of the form %s through your concrete5 website.
 
 %s
 
 To view all of this form's submissions, visit %s
 
-", $aft->getFormName(), $af->getAsText(), $formDisplayUrl);
+", $aft->getFormName(), $af->getAsText(), $formDisplayUrl));
 
 
-$bodyHTML = t("
+$bodyHTML = html_entity_decode(t("
 <p>There has been a submission of the form %s through your concrete5 website.</p>
 %s
 <p>To view all of this form's submissions, visit <a href=\"%s\">%s</a></p>
 
-", $aft->getFormName(), $af->getAsHtml(), $formDisplayUrl, $formDisplayUrl);
+", $aft->getFormName(), $af->getAsHtml(), $formDisplayUrl, $formDisplayUrl));
