50f04ac5ef48e323df80412c25d9109406f1d749
Riadh Jouili <info@concrete5-cms.de>
2016-07-06 18:33:48 +0200

    Update attribute_form_submitor_notif.php

--- mail/attribute_form_submitor_notif.php
+++ mail/attribute_form_submitor_notif.php
@@ -4,4 +4,4 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $aft           = $af->getTypeObj();
 
 $body = $aft->getFormName()."\r\n"."\r\n".$af->getAsText()."\r\n".Config::get('concrete.site')."\r\n".BASE_URL;
-$bodyHTML = "<p>" . $aft->getFormName()."</p>".$af->getAsText()."<p>".Config::get('concrete.site')."</p>".BASE_URL;
+$bodyHTML = "<p>" . $aft->getFormName()."</p>".$af->getAsHtml()."<p>".Config::get('concrete.site')."</p>".BASE_URL;
